4a3167a96ba8c41f7cc671e16088ef6f51882c64
Justin T <37750742+jtieri@users.noreply.github.com>
2019-11-27 01:20:58 -0600

    config support with yml
    Added the ability to load configs from a yml file at startup

---
+++ habbgo/utils/config.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port uint16 `yaml:"port"`
+	}
+	Database struct {
+	}
+}
+
+func LoadConfig() Config {
+	data, err := ioutil.ReadFile("./config.yml")
+	if err != nil {
+		log.Fatal("Failed to read config file 'config.yml', check that it exists & try again.", err)
+	}
+
+	config := Config{}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatal("Failed to unmarshal config file 'config.yml', check that its format is correct & try again.", err)
+	}
+
+	log.Println("The file 'config.yml' has been successfully loaded.")
+	return config
+}
