2e28054c007102c68be932a8bde99f7de5a417f9
Justin T <37750742+jtieri@users.noreply.github.com>
2019-12-03 21:13:18 -0600

    update main.go
    update

--- habbgo/main.go
+++ habbgo/main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"github.com/jtieri/HabbGo/habbgo/server"
 	"github.com/jtieri/HabbGo/habbgo/utils"
+	"log"
 )
 
 func main() {
-	//log.Println(encoding.DecodeVl64([]byte{'@','A'}))
-	//buffer := bytes.Buffer{}
-	//packet := packets.IncomingPacket{"@@", 1, buffer}
-	//packet.Payload.Write([]byte{'h','e','l','l','o'})
-	//log.Println(string(packet.Bytes()))
-	//log.Println(packet.Payload.String())
-
-
+	//log.Println(string(encoding.EncodeB64(6, 3)))
+	log.Println("Booting up HabbGo... ")
 	config := utils.LoadConfig()
-	gameServer := server.New(config.Server.Port, config.Server.Host)
+	gameServer := server.New(config.Server.Port, config.Server.Host, config.Server.MaxConns)
 	gameServer.Start()
 	defer gameServer.Stop()
 }
