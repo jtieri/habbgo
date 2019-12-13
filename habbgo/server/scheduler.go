17c6632b7876909f5a53b4ead5d10b5f783ad65a
Justin T <37750742+jtieri@users.noreply.github.com>
2019-12-13 01:20:14 -0600

    update scheduler
    Add some headers to the switch and format things a bit so its more maintainable

--- habbgo/server/scheduler.go
+++ habbgo/server/scheduler.go
@@ -2,17 +2,17 @@ package server
 
 import (
 	"github.com/jtieri/HabbGo/habbgo/game/model/player"
-	"github.com/jtieri/HabbGo/habbgo/server/protocol/handlers"
-	"github.com/jtieri/HabbGo/habbgo/server/protocol/packets"
+	"github.com/jtieri/HabbGo/habbgo/protocol/handlers"
+	"github.com/jtieri/HabbGo/habbgo/protocol/packets"
 	"log"
 )
 
 func Handle(player *player.Player, packet *packets.IncomingPacket) {
 	switch packet.HeaderId {
-	// Handshake Packets
-	case 206:
+	// Handshake Packets ----------------------------------------------------------------------------------------------
+	case 206: // INIT_CRYPTO
 		handlers.HandleInitCrypto(player, packet)
-	case 202:
+	case 202: // GENERATEKEY
 		handlers.HandleGenerateKey(player, packet)
 	case 207: // SECRETKEY
 
@@ -20,27 +20,25 @@ func Handle(player *player.Player, packet *packets.IncomingPacket) {
 
 	case 6: // UNIQUEID
 
-	case 181:
+	case 181: // GET_SESSION_PARAMETERS
 		handlers.HandleGetSessionParams(player, packet)
 	case 204: // SSO
-		// // TODO Login user if credentials are correct if not send disconnect from server
-		// Send LOGIN_OK Header:3
+		handlers.HandleSSO(player, packet)
 	case 4: // TRY LOGIN - used when SSO is disabled
-		// TODO Login user if credentials are correct if not send LOCALISED_ERROR("Incorrect Login Details.")
-		// Send LOGIN_OK Header:3
-	case 7: // GET_INFO
-		// Send USER_OBJ Header:5
-	case 8: // GET_CREDITS
-		// Send CREDIT_BALANCE
-	case 157: // Get Badges
+		handlers.HandleTryLogin(player, packet)
 
+	// Player Packets -------------------------------------------------------------------------------------------------
+	case 7: // GET_INFO
+		handlers.HandleGetInfo(player, packet)
+	case 8: // GET_CREDITS
+		handlers.HandleGetCredits(player, packet)
+	case 157: // GETAVAILABLEBADGES
+		handlers.HandleGetAvailableBadges(player, packet)
 	case 228: // Get Sound setting
 
 	case 315: // TEST_LATENCY ---> Init Latency Test
 
-
-
-
+	// ----------------------------------------------------------------------------------------------------------------
 	default:
 		log.Printf("No registered handler for packet [%v - %v], it's payload contained %v ",
 			packet.Header, packet.HeaderId, packet.Payload.String())
